d02a806379d8d5ee7dddaafe70bc40b1108f9d0b
Hannah Collins <hannahcollins53@gmail.com>
2023-04-24 12:01:54 -0700

    book 3 changes

--- honeyrae/settings.py
+++ honeyrae/settings.py
@@ -56,7 +56,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'http://172.24.48.119:3000'
 )
 
 MIDDLEWARE = [
--- repairsapi/views/employee_view.py
+++ repairsapi/views/employee_view.py
@@ -21,4 +21,4 @@ class EmployeeSerializer(serializers.ModelSerializer):
     """JSON serializer for employees"""
     class Meta:
         model = Employee
-        fields = ('id', 'user', 'specialty' )
+        fields = ('id', 'full_name', 'specialty')
